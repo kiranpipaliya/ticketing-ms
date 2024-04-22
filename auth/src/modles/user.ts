36b8be7c85aa5715faae7ffcb927898eb7e03c45
kiranpipaliya <kiranpipaliya446@gmail.com>
2024-04-22 22:41:07 +0530

    added user modal,schema,hash

---
+++ auth/src/modles/user.ts
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+import { Password } from '../services/pasword';
+
+// An interface that describe properties to new user
+interface UsersAttrs {
+	email: string;
+	password: string;
+}
+
+// An interface that describe properties that user modal has
+interface UsersModal extends mongoose.Model<UsersDocument> {
+	build(attrs: UsersAttrs): UsersDocument;
+}
+
+// An interface that describe properties that user document has
+interface UsersDocument extends mongoose.Document {
+	email: string;
+	password: string;
+}
+
+const UserSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+});
+
+UserSchema.pre('save', async function (next) {
+	if (this.isModified('password')) {
+		const hashed = await Password.toHash(this.get('password'));
+		this.set('password', hashed);
+	}
+	next();
+});
+
+UserSchema.statics.build = (attrs: UsersAttrs) => {
+	return new User(attrs);
+};
+
+const User = mongoose.model<UsersDocument, UsersModal>('User', UserSchema);
+const user = User.build({ email: '', password: '' });
+
+export { User };
